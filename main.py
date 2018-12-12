b0691bc9338d4609d5d8ebecbfcc472ad11befee
Syngard <quentin.jeromino@free.fr>
2018-12-12 11:23:51 +0100

    Video file named properlt with the movie title

--- main.py
+++ main.py
@@ -11,35 +11,47 @@ from urllib import request
 
 from menu import menu_choices 
 from request import get
-import download
+from download import download
 
 class Arte:
-    # Interface to download movies from the Arte website
+    """Interface to download movies from the Arte website"""
 
-    def __init__(self, url):
+    def __init__(self, url, title=None):
         self.url   = url
+        self.title = title
 
     def start(self):
         
         # Get page from replay website
-        resp = get(self.url)
+        resp      = get(self.url)
 
         # find API URL for the movie
-        json_url = resp[resp.find('json_url')+9:]
-        json_url = json_url[:json_url.find('"')]
-        json_url = unquote(json_url) 
+        json_url  = self.get_api_url(resp)
         
         # Get the json response from API
-        json_f   = get(json_url)
+        json_f    = get(json_url)
         
         # Parse JSON response to use properly
-        files     = json.loads(json_f)['videoJsonPlayer']['VSR']
+        files     = json.loads(json_f)['videoJsonPlayer']
         
         # Menu to choose language and resolution
-        video_url = menu_choices(files)
+        video_url = menu_choices(files['VSR'])
         
         # Download video and write to file
-        download(video_url) 
+        download(
+            video_url, 
+            filename='{filename}.mp4'.format(filename=self.get_title(files))
+        )
+
+
+    def get_api_url(self, page_html):
+        json_url = page_html[page_html.find('json_url')+9:]
+        json_url = json_url[:json_url.find('"')]
+        return unquote(json_url) 
+
+    def get_title(self, api_data):
+        return api_data['VTI']
+
 
 
 
